e9c6dcb7eece8133ca8a1408077f93b364838e03
akopowicz <anna.kopowicz@gmail.com>
2020-11-06 14:23:36 +0100

    wykonane zadanie

---
+++ js/main.js
@@ -0,0 +1,9 @@
+let paragraf = document.querySelector('p');
+
+document.getElementById("dodaj").onclick = function() {
+paragraf.textContent = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae dignissimos, dolore deserunt voluptates obcaecati quidem officia dolorem voluptatum? Debitis iste atque totam, minima ut quam unde voluptate laboriosam cum cumque.';
+};
+
+document.getElementById("usun").onclick = function() {
+    paragraf.textContent = '';
+}
